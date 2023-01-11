457d926a40a360b1bc2c7336ab5c8276cc1385ca
Divya Yeruva <divyareddy.y019@gmail.com>
2023-01-11 23:35:54 +0530

    configure webpack

---
+++ ui/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+
+
+app.use(express.static('dist'));
+app.set('port', process.env.PORT || 8080);
+
+app.get('/**', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html')
+})
+
+var server = app.listen(app.get('port'), function () {
+    console.log('listening on port ', server.address().port);
+});
