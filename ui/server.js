f0e86d0aeb0cd7dbadfc00228d65356f64786c27
Divya Yeruva <divyareddy.y019@gmail.com>
2023-01-12 16:38:22 +0530

    fix static path

--- ui/server.js
+++ ui/server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
+var path = require("path")
 
 
 
-app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname + '/dist/index.html')));
 app.set('port', process.env.PORT || 8080);
 
 app.get('/**', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.html')
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
 })
 
 var server = app.listen(app.get('port'), function () {
